7e79cb4f59e47cad193a0d8bc4d10c2954311080
hanssav <handihandi40@gmail.com>
2025-11-01 21:12:19 +0700

    shacdn/ui or https://reactnativereusables.com/
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
+import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack />
+      <PortalHost />
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,21 @@
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'expo-router';
 import { Text, View } from 'react-native';
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 justify-center items-center gap-4">
       <Text className="font-bold text-3xl text-blue-700">
         Welcome in NativeWind!
       </Text>
-      <Button
-        onPress={() => alert('Button Pressed!')}
-        // implement cn function if needed/ customize styles
-        className="bg-red-500 mt-4"
-      >
-        Click Me
+      <Button onPress={() => alert('Button Pressed!')}>
+        <Text className="text-white">Click Me</Text>
+      </Button>
+
+      <Button onPress={() => router.push('/test')}>
+        <Text className="text-white">View Component Tests</Text>
       </Button>
     </View>
   );
